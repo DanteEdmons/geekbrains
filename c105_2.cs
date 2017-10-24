312ccfd6a45458c3a3acd6b9e16002f5395f7ac4
DanteEdmons <wedoitrocks@gmail.com>
2017-10-24 18:06:55 +0300

    Update c105_2.cs

--- c105_2.cs
+++ c105_2.cs
@@ -2,6 +2,7 @@ using System;
 					
 public class Program
 {
+	
 	//создаем класс для работы со строками
 	class MyString
 	{
@@ -13,17 +14,31 @@ public class Program
 			message = this.message;
 		
 		}
+		
 		//создаем свойство, которое возвращает все слова, в которых не более n букв;
 		
 		//метод, который, удаляет из сообщения все слова, которые заканчиваются на заданный символ;
 		
 		//свойство, которое возвращает самое длинное слово сообщения;
+		public string LongestWord
+		{
+			get { return " "; }
+		}
 		
 		//свойство, которое возвращает все самые длинные слова сообщения;
 		
 		
 	}
 	
+	
+	static string GetLongestWord(string message)
+	{
+		string longestWord;
+		longestWord = "";
+		
+		
+		return longestWord;
+	}
 	
 	
 	public static void Main()
